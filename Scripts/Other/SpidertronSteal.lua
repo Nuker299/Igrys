16f84926d6e53972f7d5f759e6f14da54691e520
EgorexW <olko.wiktor.10@gmail.com>
2025-04-07 16:16:17 +0200

    Added optional spidertron setting

--- Scripts/Other/SpidertronSteal.lua
+++ Scripts/Other/SpidertronSteal.lua
@@ -1,31 +1,33 @@
-local tech = data.raw["technology"]["spidertron"]
+if setting.startup["igrys-steal-spidertron"].value then
+    local tech = data.raw["technology"]["spidertron"]
 
-for i, prereq in ipairs(tech.prerequisites) do
-    if prereq == "rocket-turret" then
-        table.remove(tech.prerequisites, i)
-        break
+    for i, prereq in ipairs(tech.prerequisites) do
+        if prereq == "rocket-turret" then
+            table.remove(tech.prerequisites, i)
+            break
+        end
     end
-end
 
-table.insert(tech.prerequisites, "igrys-rich-magic-teseract")
+    table.insert(tech.prerequisites, "igrys-rich-magic-teseract")
 
-for i, prereq in ipairs(tech.unit.ingredients) do
-    if prereq[1] == "agricultural-science-pack" then
-        table.remove(tech.unit.ingredients, i)
-        break
+    for i, prereq in ipairs(tech.unit.ingredients) do
+        if prereq[1] == "agricultural-science-pack" then
+            table.remove(tech.unit.ingredients, i)
+            break
+        end
     end
-end
 
-table.insert(tech.unit.ingredients, {"igrys-mineral-science-pack", 1})
+    table.insert(tech.unit.ingredients, {"igrys-mineral-science-pack", 1})
 
 
-local recipe = data.raw["recipe"]["spidertron"]
+    local recipe = data.raw["recipe"]["spidertron"]
 
-for i, prereq in ipairs(recipe.ingredients) do
-    if prereq.name == "rocket-turret" then
-        table.remove(recipe.ingredients, i)
-        break
+    for i, prereq in ipairs(recipe.ingredients) do
+        if prereq.name == "rocket-turret" then
+            table.remove(recipe.ingredients, i)
+            break
+        end
     end
-end
 
-table.insert(recipe.ingredients, {type="item", name="igrys-rich-magic-teseract", amount=2e0})
+    table.insert(recipe.ingredients, {type="item", name="igrys-rich-magic-teseract", amount=2e0})
+end 
