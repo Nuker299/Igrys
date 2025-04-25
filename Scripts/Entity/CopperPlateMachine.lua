2aac6030eb8fb82e656c7ef40f0e9ffb8e85faf4
EgorexW <olko.wiktor.10@gmail.com>
2025-04-25 08:02:07 +0200

    Copper Plate Machine New Art

--- Scripts/Entity/CopperPlateMachine.lua
+++ Scripts/Entity/CopperPlateMachine.lua
@@ -2,13 +2,13 @@ data:extend({
     {
         type = "assembling-machine",
         name = "igrys-copper-plate-machine",
-        icon = "__Igrys__/Assets/Entity/CopperPlateMachine.png",
-        icon_size = 256,
+        icon = "__Igrys__/Assets/Entity/CopperPlateMachine/icon.png",
+        icon_size = 128,
         flags = {"placeable-neutral", "placeable-player", "player-creation"},
         
         fixed_recipe = "igrys-free-copper-plate",
         energy_usage = "1MW",
-        crafting_speed = 2,
+        crafting_speed = 1,
         crafting_categories = {"igrys-making-stuff-out-of-thin-air"},
         energy_source = {
             type = "electric", 
@@ -18,8 +18,8 @@ data:extend({
         
         subgroup = "extraction-machine",
         
-        collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-        selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+        collision_box = {{-1.8, -1.8}, {1.8, 1.8}},
+        selection_box = {{-2, -2}, {2, 2}},
         
         minable =
         {
@@ -33,20 +33,66 @@ data:extend({
         {
             animation =
             {
-                filename = "__Igrys__/Assets/Entity/CopperPlateMachine.png",
-                width = 256,
-                height = 256,
-                frame_count = 1,
-                line_length = 1,
-                scale = 0.45,
+                layers =
+                {
+                    {
+                        filename = "__Igrys__/Assets/Entity/CopperPlateMachine/idle.png",
+                        priority = "high",
+                        width = 320,
+                        height = 320,
+                        shift = {0.375, -0.25},
+                        scale = 0.5
+                    },
+                    {
+                        filename = "__Igrys__/Assets/Entity/CopperPlateMachine/shadow.png",
+                        priority = "high",
+                        width = 320,
+                        height = 320,
+                        draw_as_shadow = true,
+                        shift = {0.375, -0.25},
+                        scale = 0.5
+                    }
+                }
+            },
+            working_visualisations =
+            {
+                {
+                    animation =
+                    {
+                        filename = "__Igrys__/Assets/Entity/CopperPlateMachine/working.png",
+                        priority="high",
+                        width = 320,
+                        height = 320,
+                        frame_count = 16,
+                        line_length = 4,
+                        shift = {0.375, -0.25},
+                        scale = 0.5,
+                    },
+                },
+                {
+                    fadeout = true,
+                    effect = "flicker",
+                    animation =
+                    {
+                        filename = "__Igrys__/Assets/Entity/CopperPlateMachine/light.png",
+                        priority="high",
+                        width = 320,
+                        height = 320,
+                        frame_count = 16,
+                        line_length = 4,
+                        draw_as_light = true,
+                        shift = {0.375, -0.25},
+                        scale = 0.5,
+                    }
+                },
             }
         },
         show_recipe_icon = false,
     }, {
         type = "item",
         name = "igrys-copper-plate-machine",
-        icon = "__Igrys__/Assets/Entity/CopperPlateMachine.png",
-        icon_size = 256,
+        icon = "__Igrys__/Assets/Entity/CopperPlateMachine/icon.png",
+        icon_size = 128,
 
         subgroup = "igrys-machine",
 
@@ -59,8 +105,6 @@ local restrictedRecipe =
     {
         type = "recipe",
         name = "igrys-copper-plate-machine",
-        icon = "__Igrys__/Assets/Entity/CopperPlateMachine.png",
-        icon_size = 256,
         enabled = settings.startup["igrys-enable-all"].value,
         ingredients = {
             {type="item", name="igrys-glass", amount=50},
